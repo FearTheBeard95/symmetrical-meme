2148707c8af4b7842c4ecd0caa1c39aa7e640231
Carlos <bigkarlbloodz@icloud.com>
2021-12-18 04:13:52 -0500

    Add Deck list view

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
 import { connect } from 'react-redux';
 import { handleReceiveDecks } from '../actions';
+import { gray, purple } from '../utils/colors';
+import DeckInfo from './DeckInfo';
 
 class DeckList extends Component {
   componentDidMount() {
     this.props.dispatch(handleReceiveDecks());
   }
   render() {
+    const { decks } = this.props;
     return (
-      <View>
-        <Text>DeckList</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}>Mobile flash cards</Text>
+        {Object.keys(decks).map((id) => {
+          return (
+            <TouchableOpacity key={id}>
+              <DeckInfo id={id} key={id} />
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
     );
   }
 }
 
-export default connect()(DeckList);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 16,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginBottom: 16,
+    color: purple,
+  },
+});
+
+function mapStateToProps(state) {
+  return {
+    decks: state,
+  };
+}
+
+export default connect(mapStateToProps)(DeckList);
